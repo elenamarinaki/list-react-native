6cf0dcfd02f4c2cb4d9b55523e151120abb950ff
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-03-30 22:21:37 +0100

    sample intro screen using style sheet

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export const App = () => {
-  return <div>App</div>;
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is a sample text</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
