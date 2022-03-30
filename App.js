04ed3172a2b4ac64cec7cabe1f0e8bafcb477e5f
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-03-31 00:59:14 +0100

    rendering a list of items

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import {Header} from './components/Header';
+// import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 364, text: 'Oat Milk'},
+    {id: 554, text: 'Bread'},
+    {id: 774, text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
